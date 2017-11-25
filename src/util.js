279a1755d9670a6a525a63a9bd853805f714e21f
wemlion <angusfu1126@qq.com>
2017-11-25 18:33:38 +0800

    update util

--- src/util.js
+++ src/util.js
@@ -4,6 +4,21 @@ export function camelCase (str) {
   return str.replace(/[_.-](\w|$)/g, (_, x) => x.toUpperCase())
 }
 
+export function kebabCase (name) {
+  return name
+    .replace(/^[A-Z]/, m => m.toLowerCase())
+    .replace(
+      /([0-9a-zA-Z])[\b\s]*([0-9A-Z])/g,
+      (m, g1, g2) => `${g1}-${g2.toLowerCase()}`
+    )
+}
+
+export function pascalCase (name) {
+  return kebabCase(name)
+    .replace(/-([0-9a-zA-Z])/g, (m, g1) => g1.toUpperCase())
+    .replace(/^[a-z]/, m => m.toUpperCase())
+}
+
 export const addESLint = (code) => `/* eslint-disable */
 ${code}
 `
@@ -51,27 +66,27 @@ ${compiled}
 }
 
 export const wrapModule = ({ componentName, compiled, css }) => {
-  return `module.exports = (function (module) {
-${compiled}
+  componentName = kebabCase(componentName)
+
+  return `/* eslint-disable */
+var moduleExports = (function (module) {
+  'use strict';
+${indent(compiled.replace(/(\/\/\n\s*)+/g, ''), '  ')}
   var exports = module.exports
   exports.name = "${componentName}"
-  exports.methods = {
-    beforeCreate: function () {
-      const css = "${css.replace(/\n/g, ' ')}"
-      if (css) {
-        this._ic_ = insert(css)
-      }
-    },
-    destroyed: function () {
-      this._ic_ && this._ic_()
+  exports.created = function () {
+    const css = "${css.replace(/\n/g, ' ')}"
+    if (css) {
+      this.____ = insert(css)
     }
+  }
+  exports.destroyed = function () {
+    this.____ && this.____()
   }
   module.exports.install = function (Vue) {
     Vue.component(exports.name, exports)
   }
-  if (typeof window !== void 0 && window.Vue) {
-    Vue.use(exports )
-  }
+
   return module.exports;
 
   function insert(css) {
@@ -89,7 +104,10 @@ ${compiled}
       document.getElementsByTagName('head')[0].removeChild(elem)
     }
   }
-  })({});
+})({});
+typeof exports === 'object' && typeof module !== 'undefined' && (module.exports = moduleExports);
+typeof window !== void 0 && window.Vue && Vue.use(moduleExports);
+this["${pascalCase(componentName)}"] = moduleExports;
 `
 }
 
