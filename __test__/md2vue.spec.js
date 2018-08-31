b613c5edf06ad084e23bc5a3b7afe6b3cf54adeb
wemlion <angusfu1126@qq.com>
2018-08-31 23:42:08 +0800

    chore: update test

--- __test__/md2vue.spec.js
+++ __test__/md2vue.spec.js
@@ -1,10 +1,11 @@
+/* eslint-disable */
 const Module = require('module')
 const { resolve } = require('path')
 const { promisify } = require('util')
 const { mount } = require('@vue/test-utils')
 const md2vue = promisify(require('../lib'))
 
-const mountApp = vfile => {
+const mountVfile = vfile => {
   const mod = new Module(vfile.path, module.parent)
   mod._compile(vfile.contents, vfile.path)
   return mount(mod.exports)
@@ -21,9 +22,9 @@ describe('md2vue', () => {
       heading: 'Not Found'
     })
 
-    const app = mountApp(file)
-    expect(app.hasClass('markdown-body')).toBe(true)
-    expect(app.contains('pre > code.language-html')).toBe(true)
-    expect(app.find('button').text()).toBe('Test Button')
+    const app = mountVfile(file)
+    expect(app.classes()).toContain('markdown-body')
+    expect(app.contains('pre.vue-demo-source-code > code.language-html')).toBe(true)
+    expect(app.find('.vue-demo > button').text()).toBe('Test Button')
   })
 })
